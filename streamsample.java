ecd1a06ba45a9b69bb2a74ad29152b43fdb2053b
shoya1002 <sho.likes.bump@gmail.com>
2023-03-18 14:28:39 +0900

    streamのコー作成しました

---
+++ streamsample.java
@@ -0,0 +1,13 @@
+package streamsample;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class streamsample {
+	public static void main(String[] args) throws Exception {
+		List<Integer> list = Arrays.asList(2, 2, 3, 4, 4, 4, 5, 7, 9);
+		list.stream()
+				.filter(i -> i >= 5)
+				.forEach(i -> System.out.println(i));
+	}
+}
